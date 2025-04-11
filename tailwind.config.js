b7a3a130190f8a6f8769ab1f2f2099648e55c0c2
thas94 <thasmalematja@gmail.com>
2025-04-11 17:47:08 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import plugin from "primevue/config";
+
+export default
+{
+    content:[
+        './pages/**/*.vue',
+        './component/**/*.vue'
+    ],
+    theme:{
+
+    },
+    plugins: [
+        'tailwindcss-primeui'
+    ]
+}
