f5444cdcf74dc9e18a4d77aa9fc8b9a8abcdf9e8
Thas94 <thasmalematja@gmail.com>
2025-04-13 22:54:18 +0200

    Added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@primevue/nuxt-module',
-],
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
 primevue: {
     options: {
         theme: 'none'
